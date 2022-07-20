aa3fff1d6d3008c8251a0ad85337e7cdf79dc596
Jason Glass <jason@broken-mold.com>
2022-07-19 22:34:40 -0500

    Log Output

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import getRoverCam from './getRoverCam.js'
+import getRoverCam from './getRoverCam.js'
 
 // Get Formatted Date
 const getDate = (offset) => {
@@ -20,4 +20,40 @@ const getDate = (offset) => {
   return `${datePart.year}-${datePart.month}-${datePart.day}`
 }
 
-console.log(getDate(10))
+
+// Get Rover Photos & Parse JSON
+const getPhotos = async (rover, camera, days) => {
+  let data = []
+
+  // Iterate Day Count
+  for (let i=0; days>i; i++) {
+
+    // Prepare Params & Get Date to Fetch
+    const params = {rover: rover, camera: camera, date: getDate(i)}
+
+    // Fetch From NASA API by Date
+    const photos = await getRoverCam(params)
+
+    // Map Image Url Strings
+    const mapPhotos = photos.map(x => {
+      return '"'+x+'"'
+    })
+
+    // Prepare Results
+    const string = '{"'+ params.date +'":['+ mapPhotos +']}'
+    const json = JSON.parse(string)
+
+    // Push Day to Array
+    data.push(json)
+  }
+
+  // Return JSON
+  return data
+}
+
+// Get Curiosity Navcam Photos
+// Last 10 Days >> 3 Photos Per Day
+const curiosity = await getPhotos('curiosity', 'NAVCAM', 10)
+
+// Log Output
+console.log(curiosity)
